ef02a5c5bb5d808f92066eee846491ea2fb053aa
johndavid400 <johndavid400@gmail.com>
2017-08-09 20:52:44 -0500

    add theme template for admin

--- app/assets/javascripts/express.js
+++
@@ -1,8 +0,0 @@
-
-$(document).on("ready", function(){
-  $("body").on("click", ".disable", function(event){
-    console.log($(this).parents(".custom-field"));
-    $(this).parents(".custom-field").remove();
-  });
-});
-
--- app/controllers/express/application_controller.rb
+++ app/controllers/express/application_controller.rb
@@ -1,11 +1,16 @@
 module Express
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :verify_admin
+    before_action :verify_admin, :set_title
+    layout 'express/theme'
 
     def verify_admin
       # override this in the host application to require some credentials
       redirect_to root_path, alert: "Admin access required" unless true
+    end
+
+    def set_title
+      @title = params[:controller].split("/").last.titleize
     end
 
   end
