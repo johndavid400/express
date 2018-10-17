7218e7411276af540a0a4f252065b2b9f02ab1f3
JD <johndavid400@gmail.com>
2018-10-16 20:54:12 -0500

    update API scope from open to available

--- app/controllers/express/api/v1/entries_controller.rb
+++ app/controllers/express/api/v1/entries_controller.rb
@@ -1,7 +1,7 @@
 class Express::Api::V1::EntriesController < Express::Api::V1::V1Controller
 
   def index
-    @resources = @site.send(klass).open
+    @resources = @site.send(klass).available
     @resources = @resources.where(channel_id: params[:channel_id]) if params[:channel_id]
     @klass = klass
     render 'express/api/v1/shared/index'
